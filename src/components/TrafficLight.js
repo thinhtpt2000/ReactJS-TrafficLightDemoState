7cea1b846fcf70e82834bd7f425af170fb42b2e6
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-28 22:09:06 +0700

    change traffic light into func component style

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import classNames from "classnames";
 import "./TrafficLight.css";
 
@@ -6,58 +6,51 @@ const RED = 0;
 const YELLOW = 1;
 const GREEN = 2;
 
-// const TrafficLight = () => {
-//   let state = {
-//     currentColor = RED
-//   };
+const TrafficLight = (props) => {
+  const { currentColor } = props;
 
-//   const getNextColor = (color) => {
-//     switch (color) {
-//       case RED:
-//         return YELLOW;
-//       case YELLOW:
-//         return GREEN;
-//       case GREEN:
-//         return RED;
-//     }
-//   };
+  return (
+    <div className="TrafficLight">
+      <div
+        className={classNames("bulb", "red", { active: currentColor === RED })}
+      />
+      <div
+        className={classNames("bulb", "yellow", {
+          active: currentColor === YELLOW,
+        })}
+      />
+      <div
+        className={classNames("bulb", "green", {
+          active: currentColor === GREEN,
+        })}
+      />
+    </div>
+  );
+};
 
-//   setInterval(() => {
-//     currentColor = getNextColor(currentColor);
-//   }, 1000);
-
-//   return (
-//     <div className="TrafficLight">
-//       <div className={classNames('bulb', 'red', {active: currentColor === RED})} />
-//       <div className={classNames('bulb', 'yellow', {active: currentColor === YELLOW})} />
-//       <div className={classNames('bulb', 'green', {active: currentColor === GREEN})} />
-//     </div>
-//   );
-// };
-
-class TrafficLight extends Component {
-  render() {
-    const { currentColor } = this.props;
-    return (
-      <div className="TrafficLight">
-        <div
-          className={classNames("bulb", "red", {
-            active: currentColor === RED,
-          })}
-        />
-        <div
-          className={classNames("bulb", "yellow", {
-            active: currentColor === YELLOW,
-          })}
-        />
-        <div
-          className={classNames("bulb", "green", {
-            active: currentColor === GREEN,
-          })}
-        />
-      </div>
-    );
-  }
-}
+// class TrafficLight extends Component {
+//   render() {
+//     const { currentColor } = this.props;
+//     return (
+//       <div className="TrafficLight">
+//         <div
+//           className={classNames("bulb", "red", {
+//             active: currentColor === RED,
+//           })}
+//         />
+//         <div
+//           className={classNames("bulb", "yellow", {
+//             active: currentColor === YELLOW,
+//           })}
+//         />
+//         <div
+//           className={classNames("bulb", "green", {
+//             active: currentColor === GREEN,
+//           })}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 export default TrafficLight;
