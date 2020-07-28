ccd19161c7fcc09a269962ca80b3aa4612980fd2
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-28 22:13:15 +0700

    change app into func component style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TrafficLight from "./components/TrafficLight";
 
@@ -6,19 +7,49 @@ const RED = 0;
 const YELLOW = 1;
 const GREEN = 2;
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentColor: RED,
-    };
-    setInterval(() => {
-      this.setState({
-        currentColor: this.getNextColor(this.state.currentColor),
-      });
-    }, 2500);
-  }
-  getNextColor(color) {
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       currentColor: RED,
+//     };
+//     setInterval(() => {
+//       this.setState({
+//         currentColor: this.getNextColor(this.state.currentColor),
+//       });
+//     }, 2500);
+//   }
+//   getNextColor(color) {
+//     switch (color) {
+//       case RED:
+//         return YELLOW;
+//       case YELLOW:
+//         return GREEN;
+//       case GREEN:
+//         return RED;
+//       default:
+//         return RED;
+//     }
+//   }
+//   render() {
+//     const { currentColor } = this.state;
+//     return (
+//       <div className="App">
+//         <TrafficLight currentColor={currentColor}/>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [color, setColor] = useState(RED);
+
+  setInterval(() => {
+    const nextColor = getNextColor(color);
+    setColor(nextColor);
+  }, 2500);
+
+  const getNextColor = (color) => {
     switch (color) {
       case RED:
         return YELLOW;
@@ -29,15 +60,12 @@ class App extends Component {
       default:
         return RED;
     }
-  }
-  render() {
-    const { currentColor } = this.state;
-    return (
-      <div className="App">
-        <TrafficLight currentColor={currentColor}/>
-      </div>
-    );
-  }
-}
+  };
+  return (
+    <div className="App">
+      <TrafficLight currentColor={color} />
+    </div>
+  );
+};
 
 export default App;
